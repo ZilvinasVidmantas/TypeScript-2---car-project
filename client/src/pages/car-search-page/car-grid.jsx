3832e538840b076b0162434061ae341b04d77fa7
labasmindaugai <labasmindaugai@gmail.com>
2021-12-19 18:38:25 +0200

    car grid styles fixed

--- client/src/pages/car-search-page/car-grid.jsx
+++ client/src/pages/car-search-page/car-grid.jsx
@@ -1,75 +1,67 @@
 import { Card, CardHeader, CardMedia, Grid } from '@mui/material';
-
-import { CarContext } from '../../contexts/car-context';
+import { Box } from '@mui/material';
 import { Link } from 'react-router-dom';
-import { styled } from '@mui/system';
-// import { Image } from 'mui-image';
+import { border, styled } from '@mui/system';
 
 const StyledCardLink = styled(Link)({
 	textDecoration: 'none',
 	color: 'black',
+	'& .textCollapse div': {
+        overflow: 'hidden',
+        whiteSpace: 'nowrap',
+    },
+    '& span': {
+        textOverflow: 'ellipsis',
+        overflow: 'hidden',
+    },
 	'& :hover div': {
-		opacity: '0.85',
+		opacity: '0.55',
 	},
 });
 
-const StyledCardMedia = styled(CardMedia)({
-	height: '250px',
-	// position: 'relative',
-	// overflow: 'hidden',
-	// ':after': {
-	// 	content: '" "',
-	// 	display: 'block',
-	// 	width: '100%',
-	// 	paddingTop: '70%',
-	// },
+const StyledCardMedia = styled('div')({
+	position: 'relative',
+	overflow: 'hidden',
+	cursor: 'pointer',
+	":after":{
+		content:'" "',
+		display: 'block',
+		width: '100%',
+		paddingBottom: '100%',
+	},
+	'>img':{
+    position: 'absolute',
+	bottom: '0',
+	left: '0',
+	right: '0',
+	width: '100%',
+	height: '100%',
+	objectFit: 'cover'
+	}
 });
 
 const CarGrid = ({ cars }) => {
 	console.log(cars);
 	return (
-		<Grid container spacing={2} sx={{ mb: 2 }}>
+		<Grid container spacing={2} sx={{ mb: 2 }} >
 			{cars.map(({ id, brand, model, year, price, images }) => {
 				return (
-					<Grid item key={id} xs={12} sm={6} md={4}>
+					<Grid item key={id} xs={12} md={6} lg={4} xl={3}>
 						<StyledCardLink to={`/car/${id}`}>
-							<Card raised>
-								<CardHeader
+						<Card raised >
+						<CardHeader
+						            className="textCollapse"
 									title={`${brand} - ${model}`}
 									subheader={`Metai: ${year} | Kaina: ${price} €`}
 									align="center"
 									sx={{ pb: 1, fontSize: '2.5vw' }}
 								/>
-								<StyledCardMedia component="div">
+								<StyledCardMedia>
 									<img
 										src={images[0]}
-										alt="car"
-										style={{
-											objectFit: 'cover',
-											width: '100%',
-											height: '100%',
-										}}
-										// duration={1000}
-										// fit="fill"
-										// style={{
-										// 	position: 'absolute',
-										// 	width: '100%',
-										// 	height: '100%',
-										// 	margingTop: '-40px',
-										// 	// paddingBottom: '20%' /* = width for a 1:1 aspect ratio */,
-										// 	// margin: '1.66%',
-										// }}
-										// style={{
-										// 	height: '100%',
-										// 	width: '100%',
-										// 	position: 'absolute',
-										// 	top: 0,
-										// 	left: 0,
-										// 	right: 0,
-										// }}
-									/>
-								</StyledCardMedia>
-							</Card>
+										alt="car"/>
+										</StyledCardMedia>
+						</Card>
 						</StyledCardLink>
 					</Grid>
 				);
