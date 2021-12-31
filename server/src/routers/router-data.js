a45b517fa11bb7ea4533f4f2b326984d76dd6a97
Ruta Bendoraityte <rbendorai@gmail.com>
2021-12-31 15:48:36 +0200

    refactor: moved router data to router-data.js

---
+++ server/src/routers/router-data.js
@@ -0,0 +1,32 @@
+const filterParamsTypes = [{
+  name: 'brand',
+  type: 'one-to-many'
+}, {
+  name: 'model',
+  type: 'one-to-many'
+}, {
+  name: 'transmission',
+  type: 'one-to-many'
+}, {
+  name: 'fuelType',
+  type: 'many-to-many'
+}, {
+  name: 'price_lte',
+  type: 'lte'
+}, {
+  name: 'price_gte',
+  type: 'gte'
+}, {
+  name: 'year_gte',
+  type: 'lte'
+}, {
+  name: 'year_lte',
+  type: 'gte'
+}];
+
+const sortingParamsNames = ['_sort_asc', '_sort_desc'];
+
+module.exports = {
+  filterParamsTypes,
+  sortingParamsNames
+}
